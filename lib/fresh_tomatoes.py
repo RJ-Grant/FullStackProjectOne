27819d3e8005c01962e35ce6e00b05729f696d37
RJ Grant <rjgrant@RJs-MacBook-Pro.local>
2017-06-12 08:43:53 -0500

    updated py files with better formatting

--- lib/fresh_tomatoes.py
+++ lib/fresh_tomatoes.py
@@ -108,7 +108,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Some of RJ's Favorite Movies</a>
           </div>
         </div>
       </div>
--- media.py
+++ media.py
@@ -1,10 +1,12 @@
 import webbrowser
 
+
 class Movie():
     """This function provides a way to create movie instances of only the
     coolest and best movies and put them on a totally rad HTML site"""
 
-    #this function initializes a Movie instance and assigns its instance variables
+    # this function initializes a Movie instance and
+    # assigns its instance variables
     def __init__(self, title, storyline, poster_image, trailer):
         self.title = title
         self.storyline = storyline
